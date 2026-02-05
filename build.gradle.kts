0b7415c053e156dd69731e9ee0224745e277df76
DFineIt <info@fine-it.cloud>
2026-02-05 16:52:45 +0100

    Remove redundant test class.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "cloud.fine-it"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
--- src/main/java/TestClass.java
+++
@@ -1,8 +0,0 @@
-import cloud.fineit.just.a1notation.A1Notation;
-
-public class TestClass {
-
-    public static void main(String... params) {
-
-    }
-}
